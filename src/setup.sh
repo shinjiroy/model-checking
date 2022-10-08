c49a19051d45e6f4a557430459a9d28aa9f5ef57
shinjiroy <shinjiroyamaguchi829@gmail.com>
2022-10-08 20:51:06 +0900

    composer installのタイミング変更、アプリケーションのenvを修正

--- src/setup.sh
+++ src/setup.sh
@@ -4,12 +4,22 @@ cd /var/www/${FRAMEWORK_DIR_NAME}/
 
 echo プロジェクトの初期設定を開始します。
 
+echo composer install...
+composer install --dev
+
+echo .envファイル作成
+cp .env.example .env
+
+echo アプリケーションキー生成
+php artisan key:generate --force
+
 echo ファイルの権限設定
 # chown dev:docker -R ./
 # chmod 775 -R ./
 chmod 777 -R ./storage/logs
+chmod 777 -R ./storage/framework
 
-echo composer install...
-composer install
+echo autoload の更新
+composer dump-autoload
 
 echo プロジェクトの初期設定が終了しました。
